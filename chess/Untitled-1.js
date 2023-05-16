a4681fd30a49dd1ed8ce9c550cf5583db1ed6241
pradeep-echortech <pradeep.p@echortech.com>
2023-05-16 09:44:19 +0530

    darg

---
+++ chess/Untitled-1.js
@@ -0,0 +1,7 @@
+// ls -> list all files and folders in directory
+// cd -> cahnge directory
+// mkdir -> create folder
+// rm -> removeEventListener(rm file_name, rm -r folder_name)
+// touch -> create file
+// nano -> text editor(vim,vi, ), nano test.txt
+// cat -> print contents of file
